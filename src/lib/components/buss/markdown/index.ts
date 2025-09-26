3027d0d6f523084c3e48b31a05a998d83d1424ae
mthezi <mthezi@163.com>
2025-09-26 11:48:32 +0800

    feat(md): remove md render

--- src/lib/components/buss/markdown/index.ts
+++ src/lib/components/buss/markdown/index.ts
@@ -1,5 +1,3 @@
 import MarkdownRenderer from "./markdown-renderer.svelte";
 
 export { MarkdownRenderer };
-
-export { markdownToHtml } from "./utils";
--- src/lib/components/buss/markdown/markdown-renderer.svelte
+++ src/lib/components/buss/markdown/markdown-renderer.svelte
@@ -1,25 +1,12 @@
 <script lang="ts">
-	import { markdownToHtml } from "./utils";
-
 	interface Props {
 		content: string;
 	}
 
 	const { content }: Props = $props();
-	let rendered = $state("");
-
-	$effect(() => {
-		if (content.trim()) {
-			markdownToHtml(content).then((html) => {
-				rendered = html;
-			});
-		} else {
-			rendered = "";
-		}
-	});
 </script>
 
 <div class="prose max-w-none">
 	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-	{@html rendered}
+	{@html content}
 </div>
