60363d447037f3b61e2942ac0f8738fe353ba4b0
JI4JUN <ji4jun0097@gmail.com>
2025-10-17 17:50:34 +0800

    build(ci): test nsis 25.42.20

--- forge.config.ts
+++ forge.config.ts
@@ -131,7 +131,7 @@ const config: ForgeConfig = {
 	makers: [
 		new MakerSquirrel(
 			{
-				name: "302-AI-Studio",
+				name: "302.AI.Studio",
 				setupIcon: "static/icon.ico",
 				iconUrl: "https://file.302.ai/gpt/playground/20250925/69b7db4b8f154fe7ad9397ba50f827b9.ico",
 				loadingGif: "static/icon.gif",
@@ -141,8 +141,9 @@ const config: ForgeConfig = {
 		{
 			name: "@felixrieseberg/electron-forge-maker-nsis",
 			config: {
-				name: "302-AI-Studio",
+				name: "302.AI.Studio",
 				executableName: "302 AI Studio",
+				artifactName: "${name}.Setup.${version}.${ext}",
 				updater: {
 					url: "https://github.com/302ai/302-AI-Studio/releases/latest/download",
 				},
