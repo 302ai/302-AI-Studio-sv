f843d76318eda8b341b1bf70ebb4e88a34ec0f04
leessmin <1442772970@qq.com>
2026-03-31 17:13:22 +0800

    style(logger): Remove unnecessary spaces from log format

--- electron/main/utils/logger.ts
+++ electron/main/utils/logger.ts
@@ -29,7 +29,7 @@ export class Logger {
 
 	protected log(level: LogLevelType, ...msg: any[]) {
 		const now = dayjs().format("HH:mm:ss.SSS");
-		console.log(`${now} `, this.formatLevel(level), this.formatModule(), `: ${msg}`);
+		console.log(`${now}`, this.formatLevel(level), this.formatModule(), `: ${msg}`);
 	}
 
 	protected formatLevel(level: LogLevelType): string {
